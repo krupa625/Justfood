4a963d7496537426ffb4f0631ac388e175d8c93d
Harsh <ph335579@gmail.com>
2024-03-17 09:26:06 +0530

    path

--- server/index.js
+++ server/index.js
@@ -1,7 +1,35 @@
-// app.js
+// // app.js
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const connectToDatabase = require('./db');
+// const authRoutes = require('./Routes/auth');
+
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// app.use('/api', authRoutes);
+// app.use('/api', require('./Routes/displayData'));
+// app.use('/api', require('./Routes/OrderData'));
+// app.get("/gett", (req,res) =>{
+//     res.send("hello");
+// })
+// connectToDatabase()
+//     .then(() => {
+//         app.listen(port, () => {
+//             console.log(`Server is running at http://localhost:${port}`);
+//         });
+//     })
+//     .catch((error) => {
+//         console.error("Error starting the application:", error.message);
+//     });
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path'); // Import path module
 const connectToDatabase = require('./db');
 const authRoutes = require('./Routes/auth');
 
@@ -11,12 +39,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Serve static files from the 'build' directory
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.use('/api', authRoutes);
 app.use('/api', require('./Routes/displayData'));
 app.use('/api', require('./Routes/OrderData'));
 app.get("/gett", (req,res) =>{
     res.send("hello");
-})
+});
+
+// Catch-all route to serve the React app's index.html file
+app.get('*', (req, res) => {
+    // Correct the path to 'index.html'
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+
 connectToDatabase()
     .then(() => {
         app.listen(port, () => {
@@ -26,3 +64,4 @@ connectToDatabase()
     .catch((error) => {
         console.error("Error starting the application:", error.message);
     });
+
